7926156cbdd481e0bfdf7f3f50eb64eb9e95a6df
shaijuapp1 <shaiju.app1@gmail.com>
2025-02-09 23:52:04 +0400

    Added build.sh script

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-# build.sh
-
 #!/bin/bash
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Apply database migrations
 python manage.py migrate
