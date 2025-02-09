a25c3b7a0897f9659f4b127aa7fd163aed245de8
shaijuapp1 <shaiju.app1@gmail.com>
2025-02-10 00:07:38 +0400

    c2

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Install dependencies
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Apply database migrations
-python manage.py migrate
+python3 manage.py migrate
 
 
